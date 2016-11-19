a89164b55693e5aa15ee47747f21b1aaabfd5716
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2016-11-18 21:05:44 -0500

    Fix interact maybe

--- src/main/java/com/thepalace/listeners/PlayerInteract.java
+++ src/main/java/com/thepalace/listeners/PlayerInteract.java
@@ -2,6 +2,7 @@ package com.thepalace.listeners;
 
 import com.thepalace.ServerPortStar;
 import com.thepalace.core.Core;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -11,7 +12,7 @@ public class PlayerInteract implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction().equals(Action.PHYSICAL)) return;
+        if (event.getClickedBlock() != null && event.getClickedBlock().getType().equals(Material.STONE_PLATE)) return;
         ServerPortStar.openPortStar(Core.getPlayerManager().getPlayer(event.getPlayer()));
     }
 }
