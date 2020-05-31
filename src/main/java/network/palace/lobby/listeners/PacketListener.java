e566a4e9f9bac905ac8a612d370ce23bbc56096b
Marc DiFilippo <legobuilder0813@gmail.com>
2020-05-31 14:05:07 -0400

    Cancel portal teleport event packet

--- src/main/java/network/palace/lobby/listeners/PacketListener.java
+++ src/main/java/network/palace/lobby/listeners/PacketListener.java
@@ -1,5 +1,11 @@
 package network.palace.lobby.listeners;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.StructureModifier;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import network.palace.core.dashboard.packets.PacketID;
@@ -9,7 +15,25 @@ import network.palace.lobby.Lobby;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.lang.reflect.Field;
+
 public class PacketListener implements Listener {
+
+    public PacketListener() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(Lobby.getInstance(), PacketType.Play.Server.WORLD_EVENT) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                StructureModifier<Integer> ints = packet.getIntegers();
+                try {
+                    Field f = ints.getField(0);
+                    f.setAccessible(true);
+                    if ((int) f.get(packet.getHandle()) == 1032) event.setCancelled(true);
+                } catch (Exception ignored) {
+                }
+            }
+        });
+    }
 
     @EventHandler
     public void onIncomingPacket(IncomingPacketEvent event) {
